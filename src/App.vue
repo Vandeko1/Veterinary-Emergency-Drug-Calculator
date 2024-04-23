040fc39ffc877ef9826c3a9381293a646effc049
Vandeko1 <vanyabodnar@gmail.com>
2024-04-23 22:20:21 +0300

    Add button clear

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,18 @@ import { ref } from 'vue';
 const drug = ref('')
 </script>
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-yellow">
-    <a href="#">
-      <img src="./assets/logo.png" alt="Logo" class="h-9">
-    </a>
-    <a href="#">На вебсайт</a>
-  </header>
   <main>
     <div class="w-screen h-screen bg-gray-200 flex justify-center items-center flex-col">
       <div class="w-1/3">
-        <h1 class="text-center mb-3 font-bold">{{ drug }}</h1>
+        <!-- <h1 class="text-center mb-3 font-bold">{{ drug.name }}</h1> -->
         <Autocomplete :source="drugs" v-model="drug" />
       </div>
       <div class="flex flex-col">
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
             <div class="overflow-hidden">
-              <table
-                class="min-w-full text-left text-sm font-light text-surface dark:text-white">
-                <thead
-                  class="border-b border-neutral-200 font-medium dark:border-white/10">
+              <table class="min-w-full text-left text-sm font-light text-surface dark:text-white">
+                <thead class="border-b border-neutral-200 font-medium dark:border-white/10">
                   <tr>
                     <th scope="col" class="px-6 py-4">Тип</th>
                     <th scope="col" class="px-6 py-4">Підтип</th>
@@ -35,10 +27,10 @@ const drug = ref('')
                 </thead>
                 <tbody>
                   <tr class="border-b border-neutral-200 dark:border-white/10">
-                    <td class="whitespace-nowrap px-6 py-4 font-medium">Обезболюючий</td>
-                    <td class="whitespace-nowrap px-6 py-4">Передопераційний</td>
-                    <td class="whitespace-nowrap px-6 py-4">1.5</td>
-                    <td class="whitespace-nowrap px-6 py-4">1.5</td>
+                    <td class="whitespace-nowrap px-6 py-4 font-medium">{{ drug.type }}</td>
+                    <td class="whitespace-nowrap px-6 py-4">{{ drug.subtype }}</td>
+                    <td class="whitespace-nowrap px-6 py-4">{{ drug.dogs }}</td>
+                    <td class="whitespace-nowrap px-6 py-4">{{ drug.cats }}</td>
                   </tr>
                 </tbody>
               </table>
--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, reactive } from 'vue';
 
 const props = defineProps({
     source: {
@@ -8,20 +8,20 @@ const props = defineProps({
         default: () => []
     },
 
-    modelValue: String
+    modelValue: Object
 })
 
 const emit = defineEmits(['update:modelValue'])
-const search = ref('')
+const search = reactive({ 'name': '' })
 
 
 const searchResults = computed(() => {
-    if (search.value === '') {
+    if (search.name === '') {
         return []
     }
 
     return props.source.filter(item => {
-        if (item.name.toLowerCase().includes(search.value.toLowerCase())) {
+        if (item.name.toLowerCase().includes(search.name.toLowerCase())) {
             return item
         }
     })
@@ -31,23 +31,34 @@ const searchResults = computed(() => {
 const isOpen = ref(false)
 const setSelected = item => {
     isOpen.value = false
-    search.value = item
-    emit('update:modelValue', search.value)
+    search.name = 'item.name'
+    emit('update:modelValue', item)
 }
 
 const handleInput = event => {
     isOpen.value = true
-    search.value = event.target.value
-    emit('update:modelValue', search.value)
+    search.name = event.target.value
+    emit('update:modelValue', search)
+}
+
+const clearInput = () => {
+    emit('update:modelValue', {})
 }
 </script>
 <template>
-    <div class="w-full relative">
-        <input type="text" :value="modelValue" @input="handleInput"
+    <div class="w-full relative input-wrapper">
+        <input type="text" :value="modelValue.name" @input="handleInput"
             class="px-5 py-3 w-full border border-gray-300 rounded-md">
+        <button v-show="search.name" @click="clearInput" class="w-9 h-9 mt-2 clear-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </button>
+
         <ul v-show="searchResults.length && isOpen"
             class="mt-1 w-full max-h-60 border border-gray-300 rounded-md bg-white absolute overflow-y-auto">
-            <li v-for="result in searchResults" :key="result.id" @click="setSelected(result.name)"
+            <li v-for="result in searchResults" :key="result.id" @click="setSelected(result)"
                 class="px-4 py-3 border-b border-gray-200 text-stone-600 cursor-pointer hover:bg-gray-100 transition-colors">
                 {{ result.name }}</li>
         </ul>
